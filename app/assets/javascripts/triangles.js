6f905c05ae4fd21790551f37a3adf551daf47885
Cody Condon <codycondon50@gmail.com>
2017-04-27 20:30:48 +1000

    remove heavy animation on start up

--- app/assets/javascripts/triangles.js
+++ app/assets/javascripts/triangles.js
@@ -10,10 +10,7 @@ $( document ).ready(function() {
     firstName.animate({"margin-left": "-110px"}, 1500);
     lastName.animate({"margin-right": "-110px"}, 1500);
 
-    $(rectangle2).animate({width: "0%"}, 2000);
 
-    $(rectangle).animate({width: "0%"}, 2000);
-    $(triangle).animate({transform: "0%"}, 2000);
 
     $(whiteBlock).animate({right: "0px"}, 3500);
     $(whiteBlock).fadeOut();
